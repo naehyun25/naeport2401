289a491d8284a1caa21c0a4cf696fdefea474a97
naehyun25 <yoon658@naver.com>
2024-04-20 17:23:31 +0900

    [240420]main text effect 테스트

--- js/nae-main-section.js
+++ js/nae-main-section.js
@@ -8,10 +8,56 @@ gsap.to(".profile-img-bg", {
         scrub: true,
         markers: true
     },
-    width: "100%",  // Animate width to 100%
-    height: "100%",  // Animate height to 100%
-    // clipPath: "none",  // Remove clipping at the end
+    width: "100%",  
+    height: "100%",  
+
     ease: "none",
     left:0,
     scale: 1.5, 
 });
+gsap.to(".back-text", {
+    scrollTrigger: {
+        trigger: ".nae-main-section",
+        start: "40px top",
+        end: "bottom bottom",
+        scrub: true,
+        markers: true
+    },
+    x: '-200%',  
+    y: '-200%', 
+    scale: 2, 
+    ease: "none"  
+});
+gsap.to(".front-text", {
+    scrollTrigger: {
+        trigger: ".nae-main-section",
+        start: "40px top",
+        end: "bottom bottom",
+        scrub: true,
+        markers: true
+    },
+    x: '200%',  
+    y: '200%', 
+    scale: 2,
+    ease: "none" 
+
+});
+gsap.to(".profile-img-wrap", {
+    scrollTrigger: {
+        trigger: ".nae-main-section",
+        start: "40px top",
+        end: "bottom bottom",
+        scrub: true,
+        markers: true
+    },
+    x: '150%',  // 전체 뷰포트의 너비만큼 오른쪽으로 이동
+
+
+});
+
+
+
+
+
+
+
